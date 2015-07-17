5b76089bab311a3e9483b730e9cbcd400cac7686
James <rockking1379@gmail.com>
2015-07-16 22:46:46 -0600

    mostly implemented. need to find a way to create file for day if doesnt exist currently if the log file already exists then it will run just fine. have not tested if it will work when the log file doesnt exist...but i suspect that it will not work

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,74 @@
 var express = require('express');
 var router = express.Router();
+var sqlite3 = require('sqlite3');
+var fs = require('fs');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var ERROR_CREATE = 'CREATE TABLE IF NOT EXISTS Error(error_id INTEGER NOT NULL,error_time DATE NOT NULL, error_message VARCHAR(100) NOT NULL,error_stacktrace TEXT NOT NULL)';
+var EVENT_CREATE = 'CREATE TABLE IF NOT EXISTS Event(event_id INTEGER NOT NULL, event_time DATE NOT NULL, event_message VARCHAR(100) NOT NULL, event_user_name VARCHAR(100) NOT NULL)';
+
+router.post('/', function(req, res) {
+  //default to HTTP OK
+  res.status(200);
+
+  //process the log
+  //if error anywhere, change status to 500
+  var log = req.body;
+  var relPathToFile = './data/db/' + log.log_date + '.db';
+  fs.realpath(relPathToFile, function(error, path){
+    if(error)
+    {
+      res.status(500);
+      console.error('file path invalid');
+    }
+    else
+    {
+      var db = new sqlite3.Database(path);
+
+      db.exec(ERROR_CREATE, function(err){
+        if(err)
+        {
+          res.status(500);
+          console.log(err);
+        }
+        else
+        {
+          db.exec(EVENT_CREATE, function(err){
+            if(err)
+            {
+              res.status(500);
+              console.log(err);
+            }
+            else
+            {
+              log.errors.forEach(function(element, index, array){
+                db.run("INSERT INTO Error(error_id, error_time, error_message, error_stacktrace) VALUES(?,?,?,?)",
+                    [element.error_id, element.error_time, element.error_message, element.error_stacktrace], function(err){
+                  if(err)
+                  {
+                    res.status(500);
+                    console.log(err);
+                  }
+                });
+              });
+
+              log.events.forEach(function(element){
+                db.run("INSERT INTO Event(event_id, event_time, event_message, event_user_name) VALUES(?,?,?,?)", [element.event_id, element.event_time, element.event_message, element.event_user_name], function(err){
+                  if(err)
+                  {
+                    res.status(500);
+                    console.log(err);
+                  }
+                });
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+
+  //send response back
+  res.send();
 });
 
 module.exports = router;
